795cbc6e385c1301002097b41a1dacbb00fa036f
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2019-06-10 23:50:08 +0200

    Fix Gauss Seidel method

--- src/main/scala/me/kcybulski/gauss/GaussSeidelRow.scala
+++ src/main/scala/me/kcybulski/gauss/GaussSeidelRow.scala
@@ -11,7 +11,7 @@ class GaussSeidelRow(A: Array[Double], b: Double, index: Int, boss: ActorRef) ex
   override def receive: Receive = LoggingReceive {
     case next: ActorRef => this.next = Option(next)
     case x: XRequest => {
-      val result = XRequest(x.old, x.current.+:(f(x)))
+      val result = XRequest(x.old, x.current.:+(f(x)))
       next.fold(boss ! result)(_ ! result)
     }
   }
